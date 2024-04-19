664c2eae96ef6fa9b741fd82be7df46cb9523189
Priyam-Pramanik <86661996+Priyam-Pramanik@users.noreply.github.com>
2024-04-19 19:04:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
-music_1 = "music.mp3";
-music_2 = "music2.mp3";
+left_wrist_score = 0;
+song_status = "";
+left_wristX = 0;
+left_wristY = 0;
+right_wristX = 0;
+right_wristY = 0;
 
 function preload(){
-    music_1 = loadSound("music.mp3");
-    music_2 = loadSound("music2.mp3");
-    left_wristX = 0;
-    left_wristY = 0;
-    right_wristX = 0;
-   right_wristY = 0;
-
+    song1 = loadSound('music.mp3');
+    song2 = loadSound('music2.mp3');
 }
+
 function setup(){
-    canvas = createCanvas(500,600);
+    canvas = createCanvas(550,500);
     canvas.center();
     
     video = createCapture(VIDEO);
     video.hide();
 
     posenet = ml5.poseNet(video, modelLoaded);
-    posenet.on('pose', gotposes);
+    posenet.on('pose', gotPoses);
 }
 
 function draw(){
-    image(video, 0, 0 , 500, 600);
+    image(video, 0, 0 , 550, 500);
+    fill('#FF0000');
+    stroke('#FF0000');
+    song_status = song1.isPlaying();
+
+    if (left_wrist_score > 0.2){
+        circle(left_wristX, left_wristY, 20);
+        song2.stop();
+    
+        if (song_status = "False"){
+            song1.play();
+            document.getElementById("song_name").innerHTML = "Song name - Harry Potter Remix";
+        }
+    }
+
+
 }
  function modelLoaded(){
     console.log("Model is loaded.");
  }
 
-function gotposes(results){
+function gotPoses(results){
     if(results.length > 0){
-        left_wristX = results[0].pose.left_wristX.x;
-        left_wristY = results[0].pose.left_wristY.y;
+        left_wristX = results[0].pose.leftWrist.x;
+        left_wristY = results[0].pose.leftWrist.y;
         console.log("Left wrist X = " + left_wristX + "Left wrist Y = " + left_wristY);
-        right_wristX = results[0].pose.right_wristX.x;
-        right_wristY = results[0].pose.right_wristY.y;
+        right_wristX = results[0].pose.rightWrist.x;
+        right_wristY = results[0].pose.rightWrist.y;
         console.log("Right wrist X = " + right_wristX + "Right wrist Y = " + right_wristY);
-
+        left_wrist_score = results[0].pose.keypoints[9].score;
+        console.log("Left wrist score = " + left_wrist_score);
     }
 
 }
