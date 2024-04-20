dda3dcec35ba5e7f518984c28ade54ccaceca2ba
Priyam-Pramanik <86661996+Priyam-Pramanik@users.noreply.github.com>
2024-04-20 11:27:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 left_wrist_score = 0;
+right_wrist_score = 0;
 song_status = "";
+song2_status = "";
 left_wristX = 0;
 left_wristY = 0;
 right_wristX = 0;
@@ -26,14 +28,24 @@ function draw(){
     fill('#FF0000');
     stroke('#FF0000');
     song_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
 
     if (left_wrist_score > 0.2){
         circle(left_wristX, left_wristY, 20);
         song2.stop();
-    
+        
         if (song_status = "False"){
             song1.play();
             document.getElementById("song_name").innerHTML = "Song name - Harry Potter Remix";
+        }
+    }
+    if (right_wrist_score > 0.2){
+        circle(right_wristX, right_wristY, 20);
+        song1.stop();
+        
+        if (song2_status = "False"){
+            song2.play();
+            document.getElementById("song_name").innerHTML = "Song name - Peter Pan";
         }
     }
 
@@ -53,6 +65,8 @@ function gotPoses(results){
         console.log("Right wrist X = " + right_wristX + "Right wrist Y = " + right_wristY);
         left_wrist_score = results[0].pose.keypoints[9].score;
         console.log("Left wrist score = " + left_wrist_score);
+        right_wrist_score = results[0].pose.keypoints[10].score;
+        console.log("Right wrist score = " + right_wrist_score);
     }
 
 }
